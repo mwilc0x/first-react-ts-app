db625887c74f8e20fb9209ec0ddc7a24152b8f95
Mike Wilcox <mwilcox56@gmail.com>
2015-10-25 14:35:20 -0400

    add react entry

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>My First React App</h1>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('app')
+);
