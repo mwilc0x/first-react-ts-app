157cf0567c9b86fa24fd94f637e53b5201e19c93
Mike Wilcox <mwilcox56@gmail.com>
2015-10-25 19:12:16 -0400

    update app with children components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import {Results} from './components/Results';
+import {Search} from './components/Search';
+import data from './data.json';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      results: []
+    }
+  }
+
+  _searchChange(e) {
+    var results = [];
+    if (e.target.value) {
+      Object.keys(data).forEach(country => {
+        const found = data[country].filter(city => {
+          return city.toLowerCase().startsWith(e.target.value.toLowerCase())
+        });
+        if (results.length < 100) {
+          results = found.reduceRight((coll,item) => {
+            coll.unshift(item);
+            return coll;
+          }, results);
+        }
+      });
+    }
+    this.setState({ results: results });
+  }
+
   render() {
     return (
-      <h1>My First React App</h1>
+      <div>
+        <h1>My First React App</h1>
+        <Search onChange={this._searchChange.bind(this)} />
+        <Results results={this.state.results} />
+      </div>
     );
   }
 }
